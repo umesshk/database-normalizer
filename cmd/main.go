fe07b8f607f8ac65752863039a16fde1b19ae31a
umesshk <umeshkumar01439@gmail.com>
2026-06-14 23:33:04 +0530

    normalizer and tests

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+
+	database "github.com/umesshk/database-normalizer/internal"
+)
+
+func Normalize(phone string) string {
+
+	var new_phone bytes.Buffer
+
+	for _, ch := range phone {
+		if ch >= '0' && ch <= '9' {
+			new_phone.WriteRune(ch)
+		}
+	}
+
+	return new_phone.String()
+}
 
 func main() {
 	fmt.Println("Helo World")
+	database.ConnectDatabase()
 }
