8c3a5e572c6b8e3b504abcc2bd17027d734a6d8b
umesshk <umeshkumar01439@gmail.com>
2026-06-14 23:37:29 +0530

    normalize using regex

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"regexp"
 
 	database "github.com/umesshk/database-normalizer/internal"
 )
 
 func Normalize(phone string) string {
-
-	var new_phone bytes.Buffer
-
-	for _, ch := range phone {
-		if ch >= '0' && ch <= '9' {
-			new_phone.WriteRune(ch)
-		}
-	}
-
-	return new_phone.String()
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
 }
+
+// func Normalize(phone string) string {
+//
+// 	var new_phone bytes.Buffer
+//
+// 	for _, ch := range phone {
+// 		if ch >= '0' && ch <= '9' {
+// 			new_phone.WriteRune(ch)
+// 		}
+// 	}
+//
+// 	return new_phone.String()
+// }
 
 func main() {
 	fmt.Println("Helo World")
