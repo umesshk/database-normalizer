38091b651d1c35a0d6c8589d75f0dbf468643bc9
umesshk <umeshkumar01439@gmail.com>
2026-06-15 17:05:01 +0530

    normatlize phone struct

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,7 @@ package main
 import (
 	"github.com/joho/godotenv"
 	database "github.com/umesshk/database-normalizer/internal"
-	"regexp"
 )
-
-func Normalize(phone string) string {
-	re := regexp.MustCompile("[^0-9]")
-	return re.ReplaceAllString(phone, "")
-}
-
-// func Normalize(phone string) string {
-//
-// 	var new_phone bytes.Buffer
-//
-// 	for _, ch := range phone {
-// 		if ch >= '0' && ch <= '9' {
-// 			new_phone.WriteRune(ch)
-// 		}
-// 	}
-//
-// 	return new_phone.String()
-// }
 
 func main() {
 	err := godotenv.Load()
--- normalizer/normalizer.go
+++ normalizer/normalizer.go
@@ -1,1 +1,21 @@
 package normalizer
+
+import "regexp"
+
+// func Normalize(phone string) string {
+//
+// 	var new_phone bytes.Buffer
+//
+// 	for _, ch := range phone {
+// 		if ch >= '0' && ch <= '9' {
+// 			new_phone.WriteRune(ch)
+// 		}
+// 	}
+//
+// 	return new_phone.String()
+// }
+
+func Normalize(phone string) string {
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
+}
